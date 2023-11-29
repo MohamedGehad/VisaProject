9f293c9526a6ee89325dc6b70416d34c1280552a
mohamed gehad <mohamed_gehad13@yahoo.com>
2023-11-29 14:40:54 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VisaProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
